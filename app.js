62afbb0817db48f5f837138f6ad8229967220f8a
Akash Das <akashd2664@gmail.com>
2024-08-12 00:22:31 +0530

    create signin-login page

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/sLogin", (req, res) => {
+  res.render("sLogin.ejs");
+});
+
+app.get("/sRegister", (req, res) => {
+  res.render("sRegister.ejs");
+});
+
+app.get("/tLogin", (req, res) => {
+  res.render("tLogin.ejs");
+});
+
+app.get("/tRegister", (req, res) => {
+  res.render("tRegister.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
